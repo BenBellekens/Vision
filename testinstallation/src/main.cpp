84185b75d3f765ba7ae0b0fba847da5d09371156
Ben Bellekens <ben.bellekens@uantwerpen.be>
2014-10-07 14:34:44 +0200

    first commit: added testinstallation

---
+++ testinstallation/src/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/visualization/cloud_viewer.h>
+
+pcl::visualization::CloudViewer viewer ("test installation");
+
+
+int main (int argc, char** argv)
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+
+  // Fill in the cloud data
+  cloud->width    = 150;
+  cloud->height   = 50;
+  cloud->is_dense = false;
+  cloud->points.resize (cloud->width * cloud->height);
+
+  for (size_t i = 0; i < cloud->points.size (); ++i)
+  {
+    cloud->points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
+    cloud->points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
+    cloud->points[i].z = 1024 * rand () / (RAND_MAX + 1.0f);
+  }
+
+   viewer.showCloud (cloud);
+   while (!viewer.wasStopped ())
+   {
+   }
+
+  return (0);
+}
