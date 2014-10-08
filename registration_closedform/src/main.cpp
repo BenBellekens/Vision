61f390257ae731c8efa58fdfd699b9bd7ef4bc88
Ben Bellekens <ben.bellekens@uantwerpen.be>
2014-10-08 22:04:43 +0200

    registrationclosed form: updates with new pointclouds

--- registration_closedform/src/main.cpp
+++ registration_closedform/src/main.cpp
@@ -7,6 +7,8 @@
 #include <pcl/common/pca.h>
 #include <pcl/common/common.h>
 #include <pcl/common/angles.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/filter.h>
 #include <pcl/registration/icp_nl.h>
 #include <pcl/registration/transforms.h>
 #include <pcl/registration/icp.h>
@@ -22,7 +24,12 @@ using namespace std;
 pcl::PointCloud<PoinT>::Ptr source_cloud (new pcl::PointCloud<PoinT>);
 pcl::PointCloud<PoinT>::Ptr target_cloud (new pcl::PointCloud<PoinT>);
 pcl::PointCloud<PoinT>::Ptr transformed_cloud (new pcl::PointCloud<PoinT>);
-pcl::visualization::PCLVisualizer viewer ("PCA registration");
+pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
+
+bool PCAregistration = false;
+bool SVDregistration = true;
+double FitnesscorePCA=0;
+double FitnesscoreSVD=0;
 
 double calculateFitnesscore(pcl::PointCloud<PoinT>::Ptr &source, pcl::PointCloud<PoinT>::Ptr &target, Eigen::Matrix4f &transformationmatrix)
 	{
@@ -67,6 +74,50 @@ double calculateFitnesscore(pcl::PointCloud<PoinT>::Ptr &source, pcl::PointCloud
 
 	}
 
+void applySVDregistration(pcl::PointCloud<PoinT>::Ptr &source, pcl::PointCloud<PoinT>::Ptr &target, Eigen::Matrix4f &transformationmatrix)
+	{
+		//center both pointcloud
+		Eigen::Vector4f _centroid_source, _centroid_target;
+		pcl::compute3DCentroid(*source, _centroid_source);
+		pcl::compute3DCentroid(*target, _centroid_target);
+
+		for (int i= 0; i < source->points.size(); ++i) {
+			source->points[i].x = source->points[i].x - _centroid_source[0];
+			source->points[i].y = source->points[i].y - _centroid_source[1];
+			source->points[i].z = source->points[i].z - _centroid_source[2];
+		}
+		for (int i= 0; i < target->points.size(); ++i) {
+			target->points[i].x = target->points[i].x - _centroid_target[0];
+			target->points[i].y = target->points[i].y - _centroid_target[1];
+			target->points[i].z = target->points[i].z - _centroid_target[2];
+		}
+		//check if the point count is equal
+		std::cout << "source points " << source->points.size() << " target points " << target->points.size() << std::endl;
+		pcl::PointCloud<PoinT>::Ptr temp (new pcl::PointCloud<PoinT>);
+		*temp = *source;
+
+		for (int i = 0; i < source->points.size(); ++i) {
+			if(i < target->points.size()){
+				temp->points[i].x = target->points[i].x;
+				temp->points[i].y = target->points[i].y;
+				temp->points[i].z = target->points[i].z;
+			}
+			else{
+				temp->points[i].x = source->points[i].x;
+				temp->points[i].y = source->points[i].y;
+				temp->points[i].z = source->points[i].z;
+			}
+		}
+		std::cout << "source points " << source->points.size() << " target points " << temp->points.size() << std::endl;
+		std::cout << "start svd estimation" << std::endl;
+		pcl::registration::TransformationEstimationSVD<PoinT, PoinT> trans_est;
+		Eigen::Matrix4f svdtransformation;
+		trans_est.estimateRigidTransformation(*source, *temp, transformationmatrix);
+
+		svdtransformation = transformationmatrix;
+
+	}
+
 void applyPCAregistration(pcl::PointCloud<PoinT>::Ptr &source, Eigen::Vector4f &centroid, Eigen::Matrix4f &transformationmatrix1)
 	{
 		//1: compute 3D centroid
@@ -76,7 +127,8 @@ void applyPCAregistration(pcl::PointCloud<PoinT>::Ptr &source, Eigen::Vector4f &
 		//5: eigenvector is the transformation matrix
 
 		std::cout << "PCA registration" << std::endl;
-
+		//pcl::PointCloud<PoinT>::Ptr temp_cloud (new pcl::PointCloud<PoinT>);
+		//*temp_cloud = *source;
 		// compute principal direction
 		Eigen::Vector4f _centroid;
 		pcl::compute3DCentroid(*source, _centroid);
@@ -84,9 +136,9 @@ void applyPCAregistration(pcl::PointCloud<PoinT>::Ptr &source, Eigen::Vector4f &
 		std::cout << "_centroid" << std::endl;
 		std::cout << _centroid << std::endl;
 		for (int i= 0; i < source->points.size(); ++i) {
-					source->points[i].x = source->points[i].x - _centroid[0];
-					source->points[i].y = source->points[i].y - _centroid[1];
-					source->points[i].z = source->points[i].z - _centroid[2];
+			source->points[i].x = source->points[i].x - _centroid[0];
+			source->points[i].y = source->points[i].y - _centroid[1];
+			source->points[i].z = source->points[i].z - _centroid[2];
 		}
 
 		Eigen::Matrix3f covariance;
@@ -118,20 +170,23 @@ void applyPCAregistration(pcl::PointCloud<PoinT>::Ptr &source, Eigen::Vector4f &
 
 	}
 
+//----------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------- 
+
 int main (int argc, char** argv)
 {
 
   //----------------------------------------------------------------------------------
   //Read pcd file
   //---------------------------------------------------------------------------------- 
-  if (pcl::io::loadPCDFile<PoinT> ("source_test_nonoise.pcd", *source_cloud) == -1) //* load the file
+  if (pcl::io::loadPCDFile<PoinT> ("Cosyslab-0.pcd", *source_cloud) == -1) //* load the file
   {
     PCL_ERROR ("Couldn't read file source_cloud.pcd \n");
     return (-1);
   }
   cout << "Loaded " << source_cloud->width * source_cloud->height << " data points "<< endl;
 
- if (pcl::io::loadPCDFile<PoinT> ("target_test_nonoise.pcd", *target_cloud) == -1) //* load the file
+ if (pcl::io::loadPCDFile<PoinT> ("Cosyslab-1.pcd", *target_cloud) == -1) //* load the file
   {
     PCL_ERROR ("Couldn't read file target_test_nonoise.pcd \n");
     return (-1);
@@ -139,7 +194,28 @@ int main (int argc, char** argv)
   cout << "Loaded " << target_cloud->width * target_cloud->height << " data points "<< endl;
 
   //----------------------------------------------------------------------------------
-  //Make source cloud Red
+  //remove NAN points from the cloud
+  //----------------------------------------------------------------------------------
+  std::vector<int> indices_src, indices_tgt;
+  pcl::removeNaNFromPointCloud(*source_cloud,*source_cloud, indices_src);
+  pcl::removeNaNFromPointCloud(*target_cloud,*target_cloud, indices_tgt);
+
+  //----------------------------------------------------------------------------------
+  //Reduce number of points
+  //----------------------------------------------------------------------------------
+  pcl::VoxelGrid<PoinT> grid, grid1;
+  grid.setLeafSize (0.01, 0.01, 0.05);
+  grid.setInputCloud (source_cloud);
+  grid.filter(*source_cloud);
+  cout << "source cloud number of point after voxelgrid: " << source_cloud->points.size() << endl;
+  
+  grid1.setLeafSize (0.01, 0.01, 0.05);
+  grid1.setInputCloud (target_cloud);
+  grid1.filter(*target_cloud);
+  cout << "target cloud number of point after voxelgrid: " << target_cloud->points.size() << endl;
+
+  //----------------------------------------------------------------------------------
+  //Make source cloud blue
   //----------------------------------------------------------------------------------
   for (int i = 0; i < source_cloud->points.size(); ++i) {
 	source_cloud->points[i].r = 0;
@@ -156,25 +232,36 @@ int main (int argc, char** argv)
   }
 	
   //----------------------------------------------------------------------------------
-  //Apply PCA transformation
+  //Apply PCA transformation from target to source
   //----------------------------------------------------------------------------------
-  
-  Eigen::Vector4f centroid_source, centroid_target;
-  Eigen::Matrix4f transformationm_source, transformationm_target, PCAtransformation;
+  if(PCAregistration == true){
+	Eigen::Vector4f centroid_source, centroid_target;
+	Eigen::Matrix4f transformationm_source, transformationm_target, PCAtransformation;
 
-  applyPCAregistration(source_cloud, centroid_source, transformationm_source);
-  applyPCAregistration(target_cloud, centroid_target, transformationm_target);
+	applyPCAregistration(source_cloud, centroid_source, transformationm_source);
+	applyPCAregistration(target_cloud, centroid_target, transformationm_target);
+	PCAtransformation = transformationm_source * transformationm_target.transpose();
 
-  PCAtransformation = transformationm_target.transpose() * transformationm_source;
+	//Apply rotation transformation 
+	pcl::transformPointCloud(*source_cloud, *transformed_cloud, PCAtransformation);
+	
+	//calculate fitnesscore
+	FitnesscorePCA = calculateFitnesscore(target_cloud,source_cloud, PCAtransformation);
+	cout << "FitnesscorePCA is : " << FitnesscorePCA  << " meter "<< endl;
+  }
 
-  //transform MISSALIGNED cloud to source_cloud
-  pcl::transformPointCloud(*target_cloud, *transformed_cloud, PCAtransformation);
+  //----------------------------------------------------------------------------------
+  //Apply SVD transformation from target to source
+  //----------------------------------------------------------------------------------
+  if(SVDregistration == true){	
+	Eigen::Matrix4f trans_matrix_svd;
+	applySVDregistration(source_cloud, target_cloud, trans_matrix_svd);
+	pcl::transformPointCloud (*source_cloud, *transformed_cloud, trans_matrix_svd);
 
-  //calculate fitnesscore
-  double fitnesscorePCA=0;
-  fitnesscorePCA = calculateFitnesscore(target_cloud,source_cloud, PCAtransformation);
-  cout << "FitnesscorePCA is : " << fitnesscorePCA  << " meter "<< endl;
-
+	//calculate fitnesscore
+ 	FitnesscoreSVD = calculateFitnesscore(target_cloud,source_cloud, trans_matrix_svd);
+	cout << "FitnesscoreSVD is : " << FitnesscoreSVD  << " meter "<< endl;
+  }
   //----------------------------------------------------------------------------------
   //Make transformed cloud White
   //----------------------------------------------------------------------------------
@@ -184,22 +271,18 @@ int main (int argc, char** argv)
 	transformed_cloud->points[i].b = 255;
   }
   	
-
   //----------------------------------------------------------------------------------
   //Show cloud in viewer
   //----------------------------------------------------------------------------------
+   viewer.showCloud (source_cloud, "source");
+   viewer.showCloud (transformed_cloud, "transformed");
+   viewer.showCloud (target_cloud, "target");
+
   while (!viewer.wasStopped ())
   {
-	viewer.addCoordinateSystem(0.1,"id",0);
-	viewer.addPointCloud(source_cloud, "source");
-	viewer.addPointCloud(transformed_cloud, "transformed");
-	viewer.addPointCloud(target_cloud, "target");
-        viewer.spin();
+	//while keypress "q" is pressed
   }
   
-
-  //pcl::io::savePCDFileASCII ("source_cloud.pcd", *source_cloud);
-  //cerr << "Saved " << source_cloud->points.size () << " data points to source_cloud.pcd." << endl;  
 
   return (0);
 }
